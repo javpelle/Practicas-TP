7a4e73ba135f0e50cb44188f60e2bb2dc14e0e0e
Luis Aguirre <luislash_13@hotmail.es>
2016-01-21 21:26:09 +0100

    Versión aceptada por el Juez ( pá k quieres saber eso jaja saludos? )

--- EDA/Entrega 26 Enero/Source.cpp
+++ EDA/Entrega 26 Enero/Source.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
-void vueltaAtrás(int S[100][100], int B[100][100], int tam, int & sol, int & solMax, int & col, bool usadas[100], int & maxSatis, int & minSatis, int satisMaxTemporal, int satisMinTemporal);
+void vueltaAtras(int S[100][100], int B[100][100], int tam, int & sol, int & solMax, int col, bool usadas[100], int maxSatis, int minSatis);
 
 int main() {
 
@@ -10,17 +11,19 @@ int main() {
 	int B[100][100];
 
 	int tam, casos;
-	int solMax = 0;
-	int col = 0;
-	int sol = 0;
-	int satisMaxTemporal=0;
-	int satisMinTemporal=0;
-	int maxSatis, minSatis;
+	int solMax, col, sol, maxSatis, minSatis;
 	bool usadas[100];
 
 	cin >> casos;
 
 	for (int i = 0; i < casos; i++){
+
+		solMax = 0;
+		col = 0;
+		sol = 0;
+		maxSatis = INT_MAX;
+		minSatis = 0;
+
 		cin >> tam;
 
 		
@@ -43,7 +46,7 @@ int main() {
 
 		
 
-		vueltaAtrás(S, B, tam, sol, solMax, col, usadas, maxSatis, minSatis, satisMaxTemporal, satisMinTemporal);
+		vueltaAtras(S, B, tam, sol, solMax, col, usadas, maxSatis, minSatis);
 		
 		if (solMax == 0) {
 			cout << "Sin solucion factible" << endl;
@@ -57,14 +60,10 @@ int main() {
 	return 0;
 }
 
-void vueltaAtrás(int S[100][100], int B[100][100], int tam, int & sol, int & solMax, int & col, bool usadas[100], int & maxSatis, int & minSatis, int satisMaxTemporal, int satisMinTemporal  ) {
+void vueltaAtras(int S[100][100], int B[100][100], int tam, int & sol, int & solMax, int col, bool usadas[100], int maxSatis, int minSatis ) {
 	for (int i = 0; i < tam; i++){
 
-	//Preparar
-		if (col == 0){
-			maxSatis = S[i][col] * 2;
-			minSatis = S[i][col] / 2;
-		}
+		
 	//Solucion
 		if (col == tam - 1){
 			if (!usadas[i] && S[i][col] < maxSatis && S[i][col] > minSatis)  {
@@ -79,27 +78,12 @@ void vueltaAtrás(int S[100][100], int B[100][100], int tam, int & sol, int & sol
 		else { 
 			if (!usadas[i] && S[i][col] < maxSatis && S[i][col] > minSatis ) {
 
-				satisMaxTemporal = maxSatis;
-				satisMinTemporal = minSatis;
-
-				if (S[i][col] * 2 < maxSatis){ //Nuevo satisfacción máximo
-					maxSatis = S[i][col] * 2;
-				} 
-
-				if (S[i][col] / 2 > minSatis){ //Nueva satisfacción minima
-					minSatis = S[i][col];
-				}
-				
 				usadas[i] = true;
 				sol += B[i][col];
-				col++;
 				
-				vueltaAtrás(S, B, tam, sol, solMax, col, usadas, maxSatis, minSatis, satisMaxTemporal, satisMinTemporal);
-				col--;
+				vueltaAtras(S, B, tam, sol, solMax, col + 1, usadas, (S[i][col] * 2 < maxSatis) ? S[i][col] * 2 : maxSatis, (S[i][col] / 2 > minSatis) ? S[i][col] / 2 : minSatis);
 				sol = sol - B[i][col];
 				usadas[i] = false;
-				maxSatis = satisMaxTemporal;
-				minSatis = satisMinTemporal;
 
 			}
 
